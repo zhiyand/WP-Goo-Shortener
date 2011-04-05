103b43e72270a7281b62c6e624a021fbdf27f7a6
7lemon <7lemonsoul@gmail.com>
2011-04-05 23:13:58 +0800

    WP-Goo-Shortener without the_goo_* functions

---
+++ lib/GooAPI.class.php
@@ -0,0 +1,59 @@
+<?php
+
+class GooAPI{
+
+	public function shorten($url){
+		$url = trim($url);
+		$postData = array(
+			'longUrl' => $url,
+			'key' => 'AIzaSyA8F0vI2rr4fwj_TTOw3d0KX_N_jzuNJA8',
+		);
+		$jsonData = json_encode($postData);
+		$curlObj = curl_init();
+		$options = array(
+			CURLOPT_URL => 'https://www.googleapis.com/urlshortener/v1/url',
+			CURLOPT_POST => 1,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_SSL_VERIFYPEER => 0,
+			CURLOPT_HEADER => 0,
+			CURLOPT_HTTPHEADER => array('Content-type:application/json'),
+			CURLOPT_POSTFIELDS => $jsonData,
+		);
+		curl_setopt_array($curlObj, $options);
+		$response = curl_exec($curlObj);
+		
+		$response_code = curl_getinfo($curlObj, CURLINFO_HTTP_CODE);
+		curl_close($curlObj);
+		if ($response_code >= 400){
+			return 'FALSE';
+		}
+		
+		$json = json_decode($response);
+		return $json->id;
+	}
+	
+	public function expend($short){
+		$short = trim($short);
+		$curlObj = curl_init();
+		$options = array(
+			CURLOPT_URL => 'https://www.googleapis.com/urlshortener/v1/url?shortUrl='.$short,
+			CURLOPT_HEADER => 0,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_SSL_VERIFYPEER => 0,
+		);
+		curl_setopt_array($curlObj, $options);
+		$response = curl_exec($curlObj);
+		
+		$response_code = curl_getinfo($curlObj, CURLINFO_HTTP_CODE);
+		curl_close($curlObj);
+		if ($response_code >= 400){
+			return 'FALSE';
+		};
+		
+		$json = json_decode($response);
+		return $json->longUrl;
+	}
+
+}
+
+?>
---
+++ wp-goo-shortener.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: WP Goo Shortener
+Plugin URI: http://www.pureweber.com
+Description: Google 短地址生成器，解析器。可以选择在文章末尾添加goo.gl短链接，并且可以在设置选项中自定义样式。如果需要将文章中的链接转换成goo.gl短链接，可以使用如下形式：[wpgoo]http://www.google.com[/wpgoo]。文章保存后该地址将会被转换成goo.gl短链接。
+Version: 0.1
+Author: 7lemon
+Author URI: http://www.7lemon.net
+*/
+
+include("lib/GooAPI.class.php");
+include("include/goo-manager.php");
+
+if(class_exists("GooManager")){
+	$GM = new GooManager();
+}
+
+if(!function_exists('add_goo_shortener_options_page')){
+	function add_goo_shortener_options_page(){
+		global $GM;
+		
+		if(!isset($GM)){
+			return;
+		}
+		
+		if (function_exists('add_options_page')){
+			add_options_page('wp-goo-shortener', 'GooShortener设置', 9, basename(__FILE__), array(&$GM, 'adminPage'));
+		}
+	}
+}
+
+function get_goo_short_url($post_id){
+	global $GM;
+	
+	return $GM->get_post_goo_url($post_id);
+}
+
+function get_goo_short_link($post_id){
+	global $GM;
+	
+	$short = $GM->get_post_goo_url($post_id);
+	$format = $GM->get_format();
+	
+	$pattern= array('/%url/is', '/%text/is');
+	$replacement = array($short, $short);
+	$link = preg_replace($pattern, $replacement, $format);
+	
+	return $link;
+}
+
+function the_goo_short_url(){
+	global $GM;
+}
+
+function the_goo_short_link(){
+	global $GM;
+}
+
+if (isset($GM)){
+	add_action('wp-goo-shortener/wp-goo-shortener.php' , array (&$GM , 'init' )) ;
+	add_action('admin_menu','add_goo_shortener_options_page');
+	add_action('save_post', array(&$GM, 'addMeta'));
+	add_action('publish_post', array(&$GM, 'convertToGooUrl'));
+}
+
+?>
